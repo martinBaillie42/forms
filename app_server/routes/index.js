91a972befaae21cb9e8839fc01bb66b573ab2770
martinbaillie42 <martin@martinbaillie.net>
2016-07-15 15:36:40 +0000

    All inputs accounted for. Not tested on various browsers yet.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,5 +5,7 @@ var ctrlForms = require('../controllers/forms');
 /* Locations pages */
 router.get('/', ctrlForms.standardInputs);
 router.post('/dummy-submit', ctrlForms.dummySubmit);
+router.post('/dummy-submit-formaction', ctrlForms.dummySubmit);
+router.get('/dummy-submit', ctrlForms.dummySubmit);
 
 module.exports = router;
